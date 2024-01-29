be2c9d2aa1cfbba2fd37fc8f466db07da9e1fed4
Jeppe Krogh <kjej@aarhus.dk>
2024-01-29 11:23:02 +0100

    Added a better way to get data from php to javascript

--- register.php
+++ register.php
@@ -5,9 +5,11 @@ use Leantime\Core\Events;
 Events::add_event_listener(
     "leantime.core.template.tpl.*.afterScriptLibTags",
 
-    function ($payload) {
+    function () {
         if (isset($_SESSION['userdata']['id']) && !is_null($_SESSION['userdata']['id'])) {
-            echo '<input type="hidden" name="userid" value="'.$_SESSION['userdata']['id'].'" />';
+            $userId = $_SESSION['userdata']['id'];
+            echo '<script>const omniSearch = '.json_encode(["settings" => ["userId" => $userId]]).'</script>';
+
             echo '<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />';
             echo '<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>';
             echo '<link rel="stylesheet" href="/dist/css/omniSearch.css"></link>';
