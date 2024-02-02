adcbcc62df67f4eda71425c0204926dd9514b929
Jeppe Krogh <kjej@aarhus.dk>
2024-02-02 11:32:38 +0100

    added settings page, saving its settings to the settings table

---
+++ Controllers/settings.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Leantime\Plugins\OmniSearch\Controllers;
+
+use Leantime\Core\Controller;
+use Leantime\Core\Frontcontroller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Leantime\Domain\Setting\Repositories\Setting as SettingRepository;
+
+/**
+ * Settings Controller for Motivational Quotes Plugin
+ *
+ * @package    leantime
+ * @subpackage plugins
+ */
+class Settings extends Controller {
+  private SettingRepository $settingsRepo;
+
+  /**
+   * constructor
+   *
+   * @access public
+   *
+   */
+  public function init(
+    SettingRepository $settingsRepo,
+  ) {
+    $this->settingsRepo = $settingsRepo;
+  }
+
+  /**
+   * get
+   *
+   * @return void
+   */
+  public function get(): Response {
+    $projectCacheExpiration = $this->settingsRepo->getSetting("omnisearchsettings.projectscache") ?: '14400';
+    $ticketCacheExpiration = $this->settingsRepo->getSetting("omnisearchsettings.ticketscache") ?: '7200';
+
+    $this->tpl->assign("projectCacheExpiration", $projectCacheExpiration);
+    $this->tpl->assign("ticketCacheExpiration", $ticketCacheExpiration);
+
+    return $this->tpl->display("omniSearch.settings");
+  }
+
+  /**
+   * post
+   *
+   * @param array $params
+   * @return void
+   */
+  public function post(array $params): RedirectResponse {
+    $this->settingsRepo->saveSetting("omnisearchsettings.projectscache", htmlspecialchars(addslashes($params['projectCacheExpiration'])));
+    $this->settingsRepo->saveSetting("omnisearchsettings.ticketscache", htmlspecialchars(addslashes($params['ticketCacheExpiration'])));
+    $this->tpl->setNotification("The settings were successfully saved.", "success");
+    return Frontcontroller::redirect(BASE_URL . "/OmniSearch/settings");
+  }
+}
