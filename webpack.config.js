3a0e2e191b3d6825b1f18b066a47f59c890f6fbd
Sine Jespersen <sinejespersen@gmail.com>
2024-07-03 13:55:04 +0200

    1739: move build to the correct folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: [ './src/omniSearch.js'],
   output: {
     // below path is assuming this plugin is installed in the leantime subfolder "plugins"
-    path: path.resolve(__dirname, './../../public/dist/js'),
+    path: path.resolve(__dirname, './../../../public/dist/js'),
     filename: 'omniSearch.js',
   },
   plugins: [
