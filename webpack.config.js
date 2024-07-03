99f1f880d4ce45dc5e0794ceb1f358951648d812
Sine Jespersen <sinejespersen@gmail.com>
2024-07-03 13:57:19 +0200

    1739: simplify webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
-const select2 = require('select2');
 
 module.exports = {
   entry: [ './src/omniSearch.js'],
@@ -11,22 +9,13 @@ module.exports = {
     filename: 'omniSearch.js',
   },
   plugins: [
-    new HtmlWebpackPlugin(),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
     }),
-    new webpack.ProvidePlugin({
-      select2: 'select2',
-    }),
   ],
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
       {
         test: /\.(sa|sc|c)ss$/,
         use: ["style-loader", "css-loader"],
