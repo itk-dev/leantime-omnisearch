84139fa6d60f85ab2e4d97e80cc5bdf466cefb5e
Sine Jespersen <sinejespersen@gmail.com>
2024-07-29 16:11:52 +0200

    1739: add TerserPlugin to webpack, to avoid comments in build and the license file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
 const path = require('path');
 const webpack = require('webpack');
+const TerserPlugin = require("terser-webpack-plugin");
 
 const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
   entry: [ './src/omniSearch.js'],
   output: {
     // below path is assuming this plugin is installed in the leantime subfolder "plugins"
