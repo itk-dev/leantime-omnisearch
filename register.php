fb52014be98b4dd7df1b59e148db6492e9567f6c
Jeppe Krogh <kjej@aarhus.dk>
2024-09-02 09:56:35 +0200

    Using renamed core file EventDispatcher

--- register.php
+++ register.php
@@ -1,12 +1,13 @@
 <?php
 
-use Leantime\Core\Events;
+use Leantime\Core\Events\EventDispatcher;
 use Leantime\Domain\Setting\Services\Setting as SettingsService;
 
-Events::add_event_listener(
+EventDispatcher::add_event_listener(
     'leantime.core.template.tpl.*.afterScriptLibTags',
     function () {
-        if (isset($_SESSION['userdata']['id']) && !is_null($_SESSION['userdata']['id'])) {
+        if (session('userdata.id') !== null) {
+            $userId  = session('userdata.id'); // you need to set the value for $userId
             $projectCacheExpiration = app()->make(SettingsService::class)->getSetting('omnisearchsettings.projectscache') ?: '2400';
             $ticketCacheExpiration = app()->make(SettingsService::class)->getSetting('omnisearchsettings.ticketscache') ?: '1200';
             echo '<script>const omniSearch = ' . json_encode(['settings' => ['userId' => $userId, 'projectCacheExpiration' => $projectCacheExpiration, 'ticketCacheExpiration' => $ticketCacheExpiration]]) . '</script>';
